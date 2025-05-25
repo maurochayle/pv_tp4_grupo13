d139e4c87e4b864996589a5db1cb6c77650460e1
maurochayle <38973588@fi.unju.edu.ar>
2025-05-25 17:27:57 -0300

    Creación estructura App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ProductForm from './components/ProductForm'
+import ProductList from './components/ProductList'
+import Searchbar from './components/Searchbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [productos, setProductos] = useState([]); //se usa para mostrar productos en ProductList, filtrar en SearchBar editar/eliminar
+  const [productosEdit, setProductoEdit] = useState(null); //se vuelve null cuando terminamos de editar o cancelar la edición
+  const [busqueda, setBusqueda] = useState(''); //ingresa valor a buscar, se usa en SearchBar ProductList
+  //Con estas 3 variables cubrimos lo que pide el tp, cargar productos nuevos, editarlos, buscarlos, borrarlos
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div>
+      <h1>Gestor de productos</h1>
+      <Searchbar/>
+      <ProductForm/>
+      <ProductList/>
+   </div>
+
   )
 }
 
